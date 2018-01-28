2128e04ead869c7b34c308ba32388261028480c4
Rainer Klute <rainer.klute@gmx.de>
2018-01-28 18:23:57 +0100

    Header boxes 4 and 5 moved from content.php to header.php.

--- content.php
+++ content.php
@@ -1,23 +1,3 @@
-<div id="header-box-4">
-    <?php
-        if (has_post_thumbnail ()) :
-            the_post_thumbnail ();
-        else :
-    ?>
-    <img alt="<?php the_title(); ?>" src="<?php header_image(); ?>">
-    <?php endif; ?>
-</div>
-<div id="header-box-5">
-    <h2>Aktuell</h2>
-    <p>Heute 19.30 Uhr</p>
-    <p>Schulungsabend</p>
-    <p>Schutz vor Gewalt und Missbrauch</p>
-    <p>Mi, 25. Jan 2017, 17.00 – 19.00 Uhr</p>
-    <p>Offenes Pastorenbüro</p>
-    <p>So, 29. Jan 2017, 17.00 Uhr</p>
-    <p>Lighthouse</p>
-    <p>Der (etwas) andere Gottesdienst</p>
-</div>
 <div id="contents-box">
     <p class="navigation">
         <a href="<?php echo get_bloginfo( 'wpurl' ); ?>">Start</a>
